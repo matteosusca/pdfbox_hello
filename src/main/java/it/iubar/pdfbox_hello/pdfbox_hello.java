fef6f7b5a33e95596c67e2fae99296b127958138
Matteo Susca <matteosusca01@gmail.com>
2018-06-14 18:23:10 +0200

    added project

---
+++ src/main/java/it/iubar/pdfbox_hello/pdfbox_hello.java
@@ -0,0 +1,10 @@
+package it.iubar.pdfbox_hello;
+
+public class pdfbox_hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+	}
+
+}
