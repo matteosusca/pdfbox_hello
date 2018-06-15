816e4c93a340b33de69c406d1922d1a419437b61
Matteo Susca <matteosusca01@gmail.com>
2018-06-15 17:30:59 +0200

    Aggiunta funzione per scrivere dati utente

--- src/main/java/it/iubar/pdfbox_hello/PdfBoxApp.java
+++ src/main/java/it/iubar/pdfbox_hello/PdfBoxApp.java
@@ -1,7 +1,10 @@
 package it.iubar.pdfbox_hello;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException; 
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.apache.pdfbox.pdmodel.*;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
@@ -21,43 +24,73 @@ public class PdfBoxApp {
 		}
 //		Add page "my_page" to document "document"
 		document.addPage(my_page);
-		PDDocumentInformation pdd = document.getDocumentInformation();
-		//Setting the author of the document
-	    pdd.setAuthor("Tutorialspoint");
-	       
-	    // Setting the title of the document
-	    pdd.setTitle("Sample document"); 
-	       
-	    //Setting the creator of the document 
-	    pdd.setCreator("PDF Examples"); 
-	       
-	    //Setting the subject of the document 
-	    pdd.setSubject("Example document");
+
+		
+		
+		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("Nome: ");
+		String name = console.readLine();
+		
+		System.out.print("Cognome: ");
+		String surname = console.readLine();
+		
+		String born[] = new String[3];
+		System.out.print("Data di nascita:\n");
+		System.out.print("	Giorno:");
+		born[0] = console.readLine();
+		System.out.print("	Mese:");
+		born[1] = console.readLine();
+		System.out.print("	Anno:");
+		born[2] = console.readLine();
+		
+		document.addPage(my_page);
+		
+		PDPage page = document.getPage(0);
+	    PDPageContentStream contentStream = new PDPageContentStream(document, page);
+	     
+	    //Insert name 
+	    contentStream.beginText();   
+	    contentStream.setFont(PDType1Font.TIMES_ITALIC, 15);
+	    contentStream.newLineAtOffset(25, 750);
+	    contentStream.showText("Nome: ");
+	    contentStream.endText();
+	    contentStream.beginText();   
+	    contentStream.setFont(PDType1Font.TIMES_BOLD, 16);
+	    contentStream.newLineAtOffset(70, 750);
+	    contentStream.showText(name);
+	    contentStream.endText();
 	    
-	    PDPage page = document.getPage(0);
-	      PDPageContentStream contentStream = new PDPageContentStream(document, page);
-	      
-	      //Begin the Content stream 
-	      contentStream.beginText(); 
-	       
-	      //Setting the font to the Content stream  
-	      contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
+	    //Insert surname 
+	    contentStream.beginText();   
+	    contentStream.setFont(PDType1Font.TIMES_ITALIC, 15);
+	    contentStream.newLineAtOffset(25, 725);
+	    contentStream.showText("Cognome: ");
+	    contentStream.endText();
+	    contentStream.beginText();   
+	    contentStream.setFont(PDType1Font.TIMES_BOLD, 16);
+	    contentStream.newLineAtOffset(95, 725);
+	    contentStream.showText(surname);
+	    contentStream.endText();
+	    
+	    //Insert born date 
+	    contentStream.beginText();   
+	    contentStream.setFont(PDType1Font.TIMES_ITALIC, 15);
+	    contentStream.newLineAtOffset(25, 700);
+	    contentStream.showText("Data di nascita: ");
+	    contentStream.endText();
+	    contentStream.beginText();   
+	    contentStream.setFont(PDType1Font.TIMES_BOLD, 16);
+	    contentStream.newLineAtOffset(130, 700);
+	    contentStream.showText(born[0] + "/" + born[1] + "/" + born[2]);
+	    contentStream.endText();
 
-	      //Setting the position for the line 
-	      contentStream.newLineAtOffset(25, 500);
+	    System.out.println("PDF personle generato");
 
-	      String text = "This is the sample document and we are adding content to it.";
-
-	      //Adding text in the form of string 
-	      contentStream.showText(text);      
-
-	      //Ending the content stream
-	      contentStream.endText();
-	      contentStream.close();
-//		Save and create a project if it doesn't exist in a path
-		document.save("C:\\Users\\Scuola - Lavoro\\Desktop\\hellociao.pdf");
-//		Close open project
-		document.close();		
+	    //Closing the content stream
+	    contentStream.close();
+		
+		document.save("C:\\Users\\Scuola - Lavoro\\Desktop\\hellociao.pdf");		
 	}
 
 }
